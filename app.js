23cecc928ebb3a843bf92391196c7c203b393eb6
Zach Auerbach <auernachzach@gmail.com>
2021-03-05 11:57:49 -0800

    refactored code from three.js documentation to make it more organized and structurted. as well as made a couple of my own methods

--- app.js
+++ app.js
@@ -1,35 +1,80 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-let camera, scene, renderer;
-let geometry, material, mesh;
+export default class Sketch {
+    constructor(options) {
+        this.time = 0;
+        this.container = options.dom;
+        this.scene = new THREE.Scene();
 
-init();
+        this.width = this.container.offsetWidth;
+        this.height = this.container.offsetHeight;
 
-function init() {
+        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
+        this.camera.position.z = 1;
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        // this.renderer.setSize( this.width, this.height );
+        // this.renderer.setAnimationLoop( animation );
+        this.container.appendChild( this.renderer.domElement );
 
-	scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement)
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
-	document.body.appendChild( renderer.domElement );
+        this.setupResize();
+        this.resize();
+        this.addObjects();
+        this.render();
+    }
 
+    setupResize() {
+        window.addEventListener('resize', this.resize.bind(this));
+    }
+
+    resize() {
+        this.width = this.container.offsetWidth;
+        this.height = this.container.offsetHeight;
+
+        this.renderer.setSize( this.width, this.height );
+
+        this.camera.aspect = this.width / this.height;
+
+    }
+
+
+    addObjects() {
+        
+    this.geometry = new THREE.BoxGeometry( 0.6, 0.6, 0.6 );
+	this.material = new THREE.MeshNormalMaterial();
+
+	this.mesh = new THREE.Mesh( this.geometry, this.material );
+	this.scene.add( this.mesh );
+
+    }
+    // Create render loop
+
+    render() {
+
+        this.time+=0.05;
+        this.mesh.rotation.x = this.time / 2000;
+	    this.mesh.rotation.y = this.time / 1000;
+
+	    this.renderer.render( this.scene, this.camera );
+        // console.log(this.time); Check out the magic of time incrementing in 0.05 ms!
+        window.requestAnimationFrame(this.render.bind(this));
+    }
 }
 
-function animation( time ) {
+new Sketch({
+    dom: document.getElementById('container')
+});
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
 
-	renderer.render( scene, camera );
 
-}
+
+// First thing to do is to add some Objects to our scene as well as a resize function.
+// Line 49-53 is our first object we are going to add.
+
+// Next we need to populate our animation loop we can take the code from three.js and
+// add it to our render function.
